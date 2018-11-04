caa9ef7e7d57551b6ea2f31a447336a938e77f5a
Guoliang Cao <gcao99@gmail.com>
2018-11-04 09:57:09 -0500

    Start from scratch, copy stuff from backup branch as needed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
 extern crate gene;
 
-use gene::parser::Parser;
-
 fn main() {
-    let str = "(defn sum [xs]
-                 (reduce + 0 xs))
-               (println (sum [1 2 3 4 5]))";
-
-    let mut parser = Parser::new(str);
-    println!("{:?}", parser.read());
-    println!("{:?}", parser.read());
 }
