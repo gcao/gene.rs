091a62c847dff4e084b7dbde1c16e2d8110672e9
Guoliang Cao <gcao99@gmail.com>
2018-11-04 17:13:38 -0500

    Get first test to pass

--- src/parser.rs
+++ src/parser.rs
@@ -22,6 +22,43 @@ impl<'a> Parser<'a> {
     }
 
     pub fn read_word(&mut self) -> Option<Result<String, Error>> {
-        return Some(Ok("".into()));
+        let start =
+            if self.pos.is_none() {
+                0
+            } else {
+                self.pos.unwrap()
+            };
+        let end = self.advance_while(|ch| !is_whitespace(ch));
+        return Some(Ok(self.str[start..end].into()));
     }
+
+    fn advance_while<F: FnMut(char) -> bool>(&mut self, mut f: F) -> usize {
+        loop {
+            match self.chars.clone().next() {
+                Some((pos, ch)) => {
+                    if f(ch) {
+                        self.next();
+                    } else {
+                        return pos;
+                    }
+                }
+                None => return self.str.len(),
+            }
+        }
+    }
+
+    fn next(&mut self) -> Option<(usize, char)> {
+        match self.chars.next() {
+            Some((pos, ch)) => {
+                self.pos = Some(pos);
+                self.chr = Some(ch);
+                return Some((pos, ch));
+            }
+            None => return None,
+        }
+    }
+}
+
+pub fn is_whitespace(ch: char) -> bool {
+    return ch.is_whitespace() || ch == ',';
 }
