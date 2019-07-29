f1bb276bd387be3fb848bfaa61313f8c6390c19d
Guoliang Cao <gcao99@gmail.com>
2019-07-29 17:44:17 -0400

    WIP: optimizing GetMember

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -128,13 +128,13 @@ impl VirtualMachine {
 
                 Instruction::SetMember(name) => {
                     self.pos += 1;
-                    let registers_temp = registers_.clone();
-                    let registers = registers_temp.borrow();
-                    let value = registers.default.clone();
+                    let value;
                     {
-                        let mut context = registers.context.borrow_mut();
-                        context.set_member(name.clone(), value);
+                        let registers_temp = registers_.clone();
+                        let registers = registers_temp.borrow();
+                        value = registers.default.clone();
                     }
+                    self.set_member(registers_.clone(), name.clone(), value);
                 }
 
                 Instruction::Jump(pos) => {
@@ -347,6 +347,33 @@ impl VirtualMachine {
         let registers_ = registers.borrow();
         let context = registers_.context.borrow();
         context.get_member(name)
+        // let mut registers_ = registers.borrow_mut();
+        // let found;
+        // {
+        //     found = registers_.members_cache.get(&name);
+        // }
+        // if found.is_some() {
+        //     Some(found.unwrap().clone())
+        // } else {
+        //     let value;
+        //     {
+        //         let context = registers_.context.borrow();
+        //         value = context.get_member(name.clone()).unwrap().clone();
+        //     }
+        //     registers_.members_cache.insert(name, value.clone());
+        //     Some(value)
+        // }
+    }
+
+    #[inline]
+    fn set_member(&self, registers: Rc<RefCell<Registers>>, name: String, value: Rc<RefCell<dyn Any>>) {
+        let registers_ = registers.borrow();
+        // let mut registers_ = registers.borrow_mut();
+        {
+            let mut context = registers_.context.borrow_mut();
+            context.set_member(name.clone(), value.clone());
+        }
+        // registers_.members_cache.insert(name, value);
     }
 }
 
@@ -357,6 +384,7 @@ pub struct Registers {
     pub context: Rc<RefCell<Context>>,
     pub cache: [Rc<RefCell<dyn Any>>; 16],
     pub store: HashMap<u16, Rc<RefCell<dyn Any>>>,
+    // pub members_cache: HashMap<String, Rc<RefCell<dyn Any>>>,
 }
 
 impl Registers {
@@ -374,6 +402,7 @@ impl Registers {
                 dummy.clone(), dummy.clone(), dummy.clone(), dummy.clone(),
             ],
             store: HashMap::new(),
+            // members_cache: HashMap::new(),
         }
     }
 
