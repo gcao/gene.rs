1c1fa2f2b07ee17243f218d940d82bb1aecf5c95
Guoliang Cao <gcao99@gmail.com>
2019-11-25 22:39:42 -0500

    Create separate binary for fibonacci

---
+++ src/bin/fibonacci.rs
@@ -0,0 +1,34 @@
+extern crate gene;
+
+use std::env;
+
+use gene::compiler2::Compiler;
+use gene::parser::Parser;
+use gene::types::Value;
+use gene::vm::VirtualMachine;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut compiler = Compiler::new();
+    let mut vm = VirtualMachine::new();
+
+    let input = format!("
+      (fn fibonacci n
+        (if (n < 2)
+          n
+        else
+          ((fibonacci (n - 1)) + (fibonacci (n - 2)))
+        )
+      )
+      (fibonacci {})
+    ", args[1]);
+    let mut parser = Parser::new(&input);
+    let parsed = parser.parse();
+    compiler.compile(parsed.unwrap());
+    let module = compiler.module;
+    let result_temp = vm.load_module(&module);
+    let borrowed = result_temp.borrow();
+    let result = borrowed.downcast_ref::<Value>().unwrap();
+    println!("Result: {}", result);
+}
